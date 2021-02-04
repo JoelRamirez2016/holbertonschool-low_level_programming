a43ab58c7e87b7adc0850706a760c4407a752786
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-04 15:07:22 -0500

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 		if (i != '9')
 		{
-			putchar(",");
-			putchar(" ");
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
