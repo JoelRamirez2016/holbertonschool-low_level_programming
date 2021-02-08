2fb4e9ae22e6b90a1e65e941695064612ec12c92
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-08 17:28:42 +0000

    file 3  0x02

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: check lowercase character 
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    printf("%i", r);
+    r = _islower('o');
+    printf("%i", r);
+    return (0);
+}
+
+int _islower(int c)
+{
+    return ( c > 96 && c < 123 );   
+}
