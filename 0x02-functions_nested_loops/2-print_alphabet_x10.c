265f5ed5b1445b4dcdb5add7378f1e1810e9c413
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-09 14:49:54 +0000

    fix 2 0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,10 @@
 void print_alphabet(void)
 {
 	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	for (i = 0; i <= 10; i++)
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
 	_putchar('\n');
 }
