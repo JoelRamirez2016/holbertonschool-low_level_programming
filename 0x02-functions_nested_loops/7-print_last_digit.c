77f8eb5865e61b32b490ca41ee993d374cdc357e
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-09 18:29:54 +0000

    Update 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,9 @@ int print_last_digit(int n)
 {
 	int result = n % 10;
 
-	_putchar(result + '0');
 	if (result < 0)
-		return (result * -1);
+		result *= -1;
+	_putchar(result + '0');
+
 	return (result);
 }
