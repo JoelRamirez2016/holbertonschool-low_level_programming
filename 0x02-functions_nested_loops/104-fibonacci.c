a3dc92e331ef63351d5a43e8276882b619ebd2d4
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-09 21:02:01 -0500

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,44 +5,40 @@
  * Description:  
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i, overflow;
-	unsigned long int nextN, n1 = 1, n2 = 2;
-	unsigned long int n1_top, n1_bot, n2_top, n2_bot, nextN_top, nextN_bot;
+	unsigned long nextN = 0, n1 = 1, n2 = 1;
+	unsigned long n1_top, n1_bot, n2_top, n2_bot, nextN_top, nextN_bot;
 
-	for (i = 0; i < 91; i++)
+	printf("1");
+	for (i = 2; i < 92; i++)
 	{
-		if (i)
-			printf(", %li", n1);
-		else
-			printf("%li", n1);
 		nextN = n1 + n2;
 		n1 = n2;
 		n2 = nextN;
+		printf(", %li", nextN);
 	}
 	n1_top = n1 / 1000000000;
 	n1_bot = n1 % 1000000000;
 	n2_top = n2 / 1000000000;
 	n2_bot = n2 % 1000000000;
 
-	for (; i < 99; i++)
-	{
+	do {
 		overflow = (n1_bot + n2_bot) / 1000000000;
-		
 		nextN_top = (n1_top + n2_top) + overflow;
-        nextN_bot = (n1_bot + n2_bot) - (1000000000 * overflow);
-        
-		printf(", %li%li", nextN_top);
+		nextN_bot = (n1_bot + n2_bot) - (1000000000 * overflow);
+
+		printf(", %lu%lu", nextN_top, nextN_bot);
 
 		n1_top = n2_top;
 		n1_bot = n2_bot;
 		n2_top = nextN_top;
 		n2_bot = nextN_bot;
-	}
-
-    // printf(", %lu%lu", nextN_top, nextN_bot);
-    
+        	i++;
+	}while (i < 99);
+	
 	printf("\n");
 	return (0);
 }
