b54ebb3741a5d62c4d92098348b96acee1d3bc2a
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-09 21:22:53 -0500

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,27 +6,24 @@
 
 void times_table(void)
 {
-	int m_er, m_cand, col_1;
+	int m_er, m_cand;
 
 	for (m_er = 0; m_er < 10; m_er++)
 	{
-		col_1 = 1;
-
 		for (m_cand = 0; m_cand < 10; m_cand++)
 		{
 			int decena = ((m_cand * m_er) / 10);
 			int unidad = ((m_cand * m_er) % 10);
 
-			if (col_1 != 1)
+			if (m_er)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			if (col_1 != 1)
 				if (decena)
 					_putchar(decena + '0');
 				else
 					_putchar(' ');
+			}
 			_putchar(unidad + '0');
 			col_1++;
 		}
