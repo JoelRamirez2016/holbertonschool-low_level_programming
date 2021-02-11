5450fae32bff095ce7c0da969470b0a84e953741
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-11 19:04:33 +0000

    fix 4 0x04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * print_numbers - print numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
