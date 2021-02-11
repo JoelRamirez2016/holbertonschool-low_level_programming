9e4dfb1fa5ab89ed56e74f29801f5aa5b16705db
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-11 16:55:19 -0500

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,8 +10,9 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
-		print_number(n * -1);
+		putchar('-');
+		print_number( (n / 10) * -1);
+		putchar(((n % 10) * -1) + '0');
 	}
 	else if (n < 10)
 		_putchar(n + '0');
