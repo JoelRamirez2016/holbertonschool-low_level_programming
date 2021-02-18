6d5c9edd46387022b9779d91df9ed6baac794518
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-18 20:44:55 +0000

    advanced 1 0x06

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_number - print a number in console
+ * @n: parameter number to print
+ * Return: the new value of s
+ */
+void print_number(int n)
+{
+	int number_h = n / 10;
+	int last_d = n % 10;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number_h = -number_h;
+		last_d = -last_d;
+	}
+
+	if (number_h)
+		print_number(number_h);
+
+	putchar(last_d + '0');
+}
+
