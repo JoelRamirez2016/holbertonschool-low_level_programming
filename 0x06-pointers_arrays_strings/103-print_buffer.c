46a0f666c209e199115b3af93bab685bd0d5798f
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-19 14:08:52 -0500

    Create 103-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i += 10, b += 10)
+	{
+		printf("%08x ", i);
+
+		for (j = 0; j < 10; j+=2)
+			if (j + i < size)
+				printf("%02x%02x ", b[j], b[j+1] );
+			else
+				printf("     ");
+
+		for (j = 0; j < 10 && j+i < size; j++)
+			printf("%c", (b[j] >= 32) ? b[j] : '.' );
+
+		printf("\n");
+	}
+}
