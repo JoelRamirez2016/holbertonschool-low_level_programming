eb2cf46a9a7981bd910eec7311e775b9a69efc02
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-22 21:19:01 +0000

    fix 2 0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,12 @@
 */
 
 char *_strchr(char *s, char c)
-{
+(
 	int i = 0;
-	char *result = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		if (s[i] == c)
-		{
-			result = s + i;
-			break;
-		}
-	return (result);
+			return (s + i);
+	return ('\0');
 }
 
