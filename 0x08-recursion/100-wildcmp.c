34208a7a87c0803e66d968dba23aee4b7a3ad68e
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-02-25 03:56:17 +0000

    fix 100 0x08

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -13,15 +13,15 @@ int wildcmp(char *s1, char *s2)
 
 		return (1);
 
-	if (*s1 == '\0' && *s2 == '*')
+	else if (*s1 == '\0' && *s2 == '*')
 
 		return (wildcmp(s1, s2 + 1));
 
-	if (*s1 == *s2)
+	else if (*s1 == *s2)
 
 		return (wildcmp((s1 + 1), (s2 + 1)));
 
-	if (*s2 == '*')
+	else if (*s2 == '*')
 		if (*(s2 + 1) == '*')
 
 			return (wildcmp(s1, s2 + 1));
@@ -33,6 +33,6 @@ int wildcmp(char *s1, char *s2)
 		else
 
 			return (wildcmp(s1 + 1, s2));
-
-	return (0);
+	else
+		return (0);
 }
