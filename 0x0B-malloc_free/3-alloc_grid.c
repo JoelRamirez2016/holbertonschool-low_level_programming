41065e2887f2b5086e8afdd418ac664ac3ab378f
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-02 14:47:14 -0500

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,11 +13,20 @@ int **alloc_grid(int width, int height)
 	int *p_array;
 	int **p_matrix;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	p_matrix = (int **)malloc(height * sizeof(int *));
+
+	if (!p_matrix)
+		return (0);
 
 	for (i = 0; i < height; i++)
 	{
 		p_array = (int *)malloc(width * sizeof(int));
+		
+		if (!p_array)
+			return (0);
 
 		for (j = 0; j < width; j++)
 			p_array[j] = 0;
@@ -26,4 +35,3 @@ int **alloc_grid(int width, int height)
 	}
 	return (p_matrix);
 }
-
