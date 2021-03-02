e82338f09ef1adc0265e4558138404839368e270
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-02 18:38:50 +0000

    fix 0 0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	array = (char *) malloc(size);
 
-        if (!size || !array)
-                return (0);
+	if (!size || !array)
+		return (0);
 
 	for (i = 0; i < size ; i++)
 		array[i] = c;
