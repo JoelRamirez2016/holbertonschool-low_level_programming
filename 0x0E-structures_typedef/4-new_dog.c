4ece3f30043c6b39ab3b233e8457d23335447ee8
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-08 18:27:32 +0000

    fix 4 0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
 
+char *_strcopy(char *src)
+{
+	int i = 0, len = 0;
+	char *dest;
+
+	while (src[i++])
+		len++;
+
+	dest = malloc(sizeof(char) * (len + 1));
+
+	if (!dest)
+		return (0);
+
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
 /**
  * new_dog - create a new dog
  * @name: name of the dog
@@ -12,9 +33,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
+	char *cp_name = _strcopy(name);
+	char *cp_owner = _strcopy(owner);
 
-	if (!my_dog)
-		return (0);
+	if (!cp_name || !cp_owner || !my_dog)
+	{
+		if (my_dog)
+			free(my_dog);
+		if (cp_name)
+			free(cp_name);
+		if (cp_owner)
+			free(cp_owner);
+		return (0);       
+	}
 
 	my_dog->name = name;
 	my_dog->age = age;
