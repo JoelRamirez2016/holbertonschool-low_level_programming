256c1557f5431a82ecb1bf5722a730af44a27072
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-11 18:59:17 +0000

    fix 3 0x10 delimiter

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,6 +5,11 @@ void _printi(va_list);
 void _prints(va_list);
 void _printf(va_list);
 
+/**
+ * print_all - print any type of variable
+ * @format: types of the vars to print
+*/
+
 void print_all(const char * const format, ...)
 {
 	op_t ops[] = {
@@ -16,8 +21,9 @@ void print_all(const char * const format, ...)
 	};
 	va_list args;
 	int i = 0, j = 0;
+	char *delimiter = "";
 
-	va_start (args, format);
+	va_start(args, format);
 
 	while (format[i])
 	{
@@ -26,31 +32,56 @@ void print_all(const char * const format, ...)
 		{
 			if (ops[j].op == format[i])
 			{
+				printf("%s", delimiter);
 				ops[j].f(args);
-				printf(", ");
+				delimiter = ", ";
 			}
 			j++;
 		}
 		i++;
 	}
 
-	va_end (args);
+	va_end(args);
 
 	printf("\n");
 }
+
+/**
+ * _printi - print integer of the list of arguments
+ * @a: list of arguments
+*/
 
 void _printi(va_list a)
 {
 	printf("%i", va_arg(a, int));
 }
+
+/**
+ * _printf - print float of the list of arguments
+ * @a: list of arguments
+*/
+
 void _printf(va_list a)
 {
 	printf("%f", va_arg(a, double));
 }
+
+/**
+ * _printc - print character of the list of arguments
+ * @a: list of arguments
+*/
+
 void _printc(va_list a)
 {
 	printf("%c", va_arg(a, int));
 }
+
+/**
+ * _prints - print string of the list of arguments
+ * or nil if no arguments
+ * @a: list of arguments
+*/
+
 void _prints(va_list a)
 {
 	char *s = va_arg(a, char*);
