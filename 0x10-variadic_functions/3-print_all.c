ef6bee0354ef6a3dd21203f20aaf36628d8a96d8
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-11 18:25:49 +0000

    fix 3 0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,31 +1,61 @@
 #include "variadic_functions.h"
 
+void _printc(va_list);
 void _printi(va_list);
 void _prints(va_list);
+void _printf(va_list);
 
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    
-    va_start (args, format);
-    
-    while (format[i])
-    {
-        _printi(args);
-        printf("\n");
-        i++;
-    }
-    
-    va_end (args);                  
+	op_t ops[] = {
+		{'c', _printc},
+		{'i', _printi},
+		{'f', _printf},
+		{'s', _prints},
+		{0, 0}
+	};
+	va_list args;
+	int i = 0, j = 0;
 
+	va_start (args, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (ops[j].op)
+		{
+			if (ops[j].op == format[i])
+			{
+				ops[j].f(args);
+				printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end (args);
+
+	printf("\n");
 }
 
 void _printi(va_list a)
 {
-    printf("%i", va_arg(a, int));
+	printf("%i", va_arg(a, int));
+}
+void _printf(va_list a)
+{
+	printf("%f", va_arg(a, float));
+}
+void _printc(va_list a)
+{
+	printf("%c", va_arg(a, int));
 }
 void _prints(va_list a)
 {
-    printf("%s", va_arg(a, char*));
+	char *s = va_arg(a, char*);
+
+	if (!*s)
+		s = "(nil)";
+	printf("%s", s);
 }
