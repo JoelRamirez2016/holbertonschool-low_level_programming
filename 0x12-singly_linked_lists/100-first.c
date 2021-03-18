2f59ca06ba462837d57d1836d6e73214eb9f17b9
vagrant <vagrant@precise32.(none)>
2021-03-18 20:18:07 +0000

    advanced files up

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * prepare - print a string before the main execute
+ */
+
+void __attribute__ ((constructor)) prepare()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
