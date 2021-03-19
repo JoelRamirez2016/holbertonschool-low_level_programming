f52ad259a66aa9d49a17b3521b3380f545901321
vagrant <vagrant@precise32.(none)>
2021-03-19 15:37:05 +0000

    betty .h fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <stdio.h>
 
-/* struct list_s - singly linked list
+/**
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -13,11 +14,12 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
