e67d5007f3fec7b16b443a9c2f8bfd947bf75a03
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-23 16:35:30 +0000

    fix 101 0x13 size

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,6 +20,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		printf("[%p] %i\n", (void *) head, head->n);
 		printf("-> [%p] %i\n", (void *) head->next, (head->next)->n);
+		size++;
 	}
 	return (size);
 }
