5a33d4782a9e1a4d4df924f7e278a5431ed81c9b
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-03-29 19:33:13 +0000

    file 1 0x15 up and .h fun

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file and his content
+ * @filename: pointer to the name of the file.
+ * @text_content: content of the new file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t opening, writing, letters = 0;
+
+	while (text_content && text_content[letters])
+		letters++;
+
+	opening = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+	writing = write(opening, text_content, letters);
+
+	close(opening);
+
+	return (writing);
+}
