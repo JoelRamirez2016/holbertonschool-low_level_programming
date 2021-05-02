b36c8d63e56871344789504154eab8f861fe89d2
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-02 19:40:04 +0000

    fix 7 0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *h;
-/*		(*h)->prev = new;*/
+		if (*h)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
