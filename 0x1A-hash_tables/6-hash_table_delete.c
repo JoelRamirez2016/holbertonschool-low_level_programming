15b2b20020650a8a56a94c85a081d27016171604
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-28 14:17:32 +0000

    fix betty 6 0x1A

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,16 +9,16 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned int i;
 	hash_node_t *curr;
 
-	for(i = 0; i < ht->size; i++)
-		while(ht->array[i])
+	for (i = 0; i < ht->size; i++)
+		while (ht->array[i])
 		{
 			free(ht->array[i]->key);
 			free(ht->array[i]->value);
 			curr = ht->array[i];
 			ht->array[i] = curr->next;
-			free(curr);				
+			free(curr);
 		}
-	
+
 	free(ht->array);
 	free(ht);
 }
