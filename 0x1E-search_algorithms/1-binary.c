a902665505f2bbb6dd41c8dca11ab0be8e55be17
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-01 19:28:34 +0000

    fix 1 0x1E

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,6 @@
 #include "search_algos.h"
 
-void print_array (int *array, size_t size);
-/*int recursive_binary_search(int *array, size_t index, size_t size, int value);*/
+void print_array(int *array, size_t size);
 
 /**
  * binary_search - searches for a value in a sorted array of integers
@@ -19,28 +18,18 @@ int binary_search(int *array, size_t size, int value)
 	print_array(array, size);
 
 	if (value < array[size / 2])
-		return (binary_search(array, size / 2 - 1, value));
+		return (binary_search(array, size / 2 + 1, value));
 	else if (value > array[size / 2])
-		return (binary_search(array + size / 2 + 1, size - size / 2 - 1, value));
+		return (binary_search(array + size / 2 + 1, size - size / 2 - 1,  value));
 	return (size / 2);
 }
-/*
-int recursive_binary_search(int *array, size_t index, size_t size, int value)
-{
-	if (size == 0)
-		return (-1);
 
-	print_array(array, size);
-
-	if (value < array[size / 2])
-		return (recursive_binary_search(array, 0, index, value));
-	else if (value > array[size / 2])
-		return (recursive_binary_search(array + size / 2, size / 2, size, value));
-	else
-		return (index);
-}
-*/
-void print_array (int *array, size_t size)
+/**
+ * print_array - print array in stdout
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ */
+void print_array(int *array, size_t size)
 {
 	size_t i;
 
@@ -51,7 +40,7 @@ void print_array (int *array, size_t size)
 		printf(" %i", array[i]);
 
 		if (i + 1 < size)
-			printf(",");			
+			printf(",");
 	}
 	printf("\n");
 }
